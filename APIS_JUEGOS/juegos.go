9339041f091e221563b62eedf2d82e2eb1053314
mdmata20 <matalejandro18@gmail.com>
2021-10-24 22:53:03 -0600

    juegos de squitGame

---
+++ APIS_JUEGOS/juegos.go
@@ -0,0 +1,101 @@
+package main
+
+
+import (
+	"fmt"
+	
+	"math/rand"
+	
+	
+	"time"
+)
+
+
+/* juego 1 */
+func juego1(jugadores int) int {
+	rand.Seed(time.Now().UnixNano())
+
+	var ganador []int
+	
+	fmt.Println("--------- juego 1 ----------")
+
+	fmt.Println("jugadores jugando")
+
+	for i := 1; i <= jugadores; i++ {
+		ganador = append(ganador,i)
+	}
+
+	fmt.Println("El ganador es: ")
+
+	ganador1 := rand.Intn(len(ganador))
+	Campeon := ganador[ganador1]
+	
+
+	fmt.Println(Campeon)
+	return Campeon
+}
+
+/* juego 2 */
+func juego2(jugadores int) int {
+	rand.Seed(time.Now().UnixNano())
+
+	var ganador []int
+	fmt.Println("--------- juego 2 ----------")
+
+	fmt.Println("jugadores jugando")
+
+	for i := 1; i <= jugadores; i++ {
+		
+		if(i%2 == 1){
+			ganador = append(ganador,i)
+			
+		}
+
+	}
+
+	fmt.Println("El ganador es: ")
+	ganador2 := rand.Intn(len(ganador))
+	campeon2 := ganador[ganador2]
+
+	fmt.Println(campeon2)
+
+	return campeon2
+
+}
+
+/* juego 3 */
+func juego3(jugadores int) int {
+	rand.Seed(time.Now().UnixNano())
+
+	var ganador []int
+	fmt.Println("--------- juego 2 ----------")
+
+	fmt.Println("jugadores jugando")
+
+	for i := 1; i <= jugadores; i++ {
+		
+		if(i%2 == 0){
+			ganador = append(ganador,i)
+			
+		}
+
+	}
+
+	fmt.Println("El ganador es: ")
+	ganador2 := rand.Intn(len(ganador))
+	campeon2 := ganador[ganador2]
+
+	fmt.Println(campeon2)
+
+	return campeon2
+
+}
+
+
+
+
+func main() {
+	juego3(20)
+	//juego2(10)
+	//juego1(20)
+}
